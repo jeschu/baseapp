a3ac4f1d2ea81cc753362c4e762014b0f31e6669
Jens Schumacher <jens.schumacher@bundesanzeiger.de>
2026-04-19 20:20:57 +0200

    repository: refactor TableRequestRepository and implementation
    - Updated TableRequestRepository interface to extend only TableRequestRepositoryCustom.
    - Renamed TableRequestRepositoryImpl to TableRequestRepositoryCustomImpl and changed annotation from @Repository to @Component.
    - Modified TrackService to use the new search method in trackOverviewRepository.

--- src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
+++ src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
@@ -3,10 +3,10 @@ package info.maila.baseapp.database
 import info.maila.baseapp.common.rest.TablePageable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Component
 
-@Repository
-class TableRequestRepositoryImpl<T : Any> : TableRequestRepositoryCustom<T> {
+@Component
+class TableRequestRepositoryCustomImpl<T : Any> : TableRequestRepositoryCustom<T> {
     override fun search(pageable: TablePageable): Page<T> {
         // Leere Implementierung, kann später angepasst werden
         return PageImpl(emptyList<T>(), pageable, 0)
