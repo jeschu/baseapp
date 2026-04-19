690984cd07bec232bbe589fbf0e02c3108581a0e
Jens Schumacher <jens.schumacher@bundesanzeiger.de>
2026-04-19 19:45:32 +0200

    repository: Add NoRepositoryBean annotation to TableRequestRepository interfaces
    - Added @NoRepositoryBean annotation to TableRequestRepository and TableRequestRepositoryCustom interfaces.
    - Updated TableRequestRepositoryCustomImpl to include @Repository annotation.

--- src/main/kotlin/info/maila/baseapp/database/TableRequestRepository.kt
+++ src/main/kotlin/info/maila/baseapp/database/TableRequestRepository.kt
@@ -1,6 +1,8 @@
 package info.maila.baseapp.database
 
+import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.data.repository.PagingAndSortingRepository
 
+@NoRepositoryBean
 interface TableRequestRepository<T : Any, ID : Any>
     : PagingAndSortingRepository<T, ID>, TableRequestRepositoryCustom<T, ID>
--- src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
+++ src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
@@ -3,16 +3,20 @@ package info.maila.baseapp.database
 import info.maila.baseapp.common.rest.TablePageable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
+import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 
 @Component
 class TableRequestRepositoryCustomImpl<T : Any, ID : Any>(
     private val jdbcTemplate: NamedParameterJdbcTemplate
 ) : TableRequestRepositoryCustom<T, ID> {
+
     override fun findAll(rq: TablePageable): Page<T> {
         // Dummy-Implementierung: gibt eine leere Seite zurück
         // Hier solltest du die eigentliche Query generisch implementieren
         return PageImpl(emptyList<T>())
     }
+
 }
