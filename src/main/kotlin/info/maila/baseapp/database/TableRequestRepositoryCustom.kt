5c3a78dc692b28ba3effeceda6c4da2c0e0a8561
Jens Schumacher <jens.schumacher@bundesanzeiger.de>
2026-04-19 19:37:33 +0200

    repository: Remove NoRepositoryBean annotation and implement custom repository
    - Removed @NoRepositoryBean annotation from TableRequestRepository and TableRequestRepositoryCustom interfaces.
    - Updated TableRequestRepositoryCustomImpl to use NamedParameterJdbcTemplate and annotated it with @Component.
    - Implemented a dummy findAll method in TableRequestRepositoryCustomImpl that returns an empty Page.

--- src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustom.kt
+++ src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustom.kt
@@ -2,9 +2,7 @@ package info.maila.baseapp.database
 
 import info.maila.baseapp.common.rest.TablePageable
 import org.springframework.data.domain.Page
-import org.springframework.data.repository.NoRepositoryBean
 
-@NoRepositoryBean
 interface TableRequestRepositoryCustom<T : Any, ID : Any> {
 
     fun findAll(rq: TablePageable): Page<T>
--- src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
+++ src/main/kotlin/info/maila/baseapp/database/TableRequestRepositoryCustomImpl.kt
@@ -2,11 +2,17 @@ package info.maila.baseapp.database
 
 import info.maila.baseapp.common.rest.TablePageable
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Component
 
-class TableRequestRepositoryCustomImpl<T : Any, ID : Any> : TableRequestRepositoryCustom<T, ID> {
-
+@Component
+class TableRequestRepositoryCustomImpl<T : Any, ID : Any>(
+    private val jdbcTemplate: NamedParameterJdbcTemplate
+) : TableRequestRepositoryCustom<T, ID> {
     override fun findAll(rq: TablePageable): Page<T> {
-        TODO("Not yet implemented")
+        // Dummy-Implementierung: gibt eine leere Seite zurück
+        // Hier solltest du die eigentliche Query generisch implementieren
+        return PageImpl(emptyList<T>())
     }
-
 }
