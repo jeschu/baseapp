39e072e503fe805e041ffd965480d781f3f57a2e
Jens Schumacher <jens.schumacher@bundesanzeiger.de>
2026-04-20 21:20:15 +0200

    refactor: rename EntityFieldCache to EntityCache and improve caching logic
    - Renamed EntityFieldCache.kt to EntityCache.kt for clarity.
    - Updated caching structure to separate table and field caches.
    - Enhanced the get method to retrieve persistent entities more efficiently.
    - Improved SQL query construction in TablePageableRepositoryImpl for better readability and performance.

--- src/main/kotlin/info/maila/baseapp/database/repository/EntityCache.kt
+++ src/main/kotlin/info/maila/baseapp/database/repository/EntityCache.kt
@@ -3,24 +3,31 @@ package info.maila.baseapp.database.repository
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.data.mapping.PersistentProperty
 import org.springframework.data.relational.core.mapping.RelationalMappingContext
+import org.springframework.data.relational.core.mapping.RelationalPersistentEntity
 import org.springframework.stereotype.Component
 import kotlin.reflect.KClass
 
 @Component
-class EntityFieldCache(private val mappingContext: RelationalMappingContext) {
+class EntityCache(private val mappingContext: RelationalMappingContext) {
 
     private val logger = KotlinLogging.logger { }
 
-    private val cache: MutableMap<KClass<*>, MutableMap<String, EntityField>> = mutableMapOf()
+    private val tableCache: MutableMap<KClass<*>, RelationalPersistentEntity<*>> = mutableMapOf()
+    private val fieldCache: MutableMap<KClass<*>, MutableMap<String, EntityField>> = mutableMapOf()
 
-    fun get(entityClass: KClass<*>, field: String): EntityField = cache
+    fun get(entityClass: KClass<*>) = tableCache
         .computeIfAbsent(entityClass) {
-            mutableMapOf()
+            mappingContext.getPersistentEntity(it.java)
+                ?: throw IllegalStateException("No persistent entity found for class ${entityClass.qualifiedName}")
+
         }
+
+    fun get(entityClass: KClass<*>, field: String): EntityField = fieldCache
+        .computeIfAbsent(entityClass) { mutableMapOf() }
         .computeIfAbsent(field) {
-            entityField(entityClass, field).also {
-                logger.trace { "reflected ${entityClass.simpleName}.$field -> column='${it.column}', isString=${it.isString}" }
-            }
+            val entityField = entityField(entityClass, field)
+            logger.trace { "reflected ${entityClass.simpleName}.$field -> column='${entityField.column}', isString=${entityField.isString}" }
+            entityField
         }
 
     private fun entityField(entityClass: KClass<*>, field: String): EntityField {
@@ -30,11 +37,7 @@ class EntityFieldCache(private val mappingContext: RelationalMappingContext) {
         return EntityField(entityClass, field, column, isString)
     }
 
-    private fun KClass<*>.getPersistentProperty(field: String) = this
-        .let {
-            mappingContext.getPersistentEntity(it.java)
-                ?: throw IllegalStateException("No persistent entity found for class $qualifiedName")
-        }
+    private fun KClass<*>.getPersistentProperty(field: String) = get(this)
         .let {
             it.getPersistentProperty(field)
                 ?: throw IllegalStateException("No persistent property found for field '$field' in class $qualifiedName")
--- src/main/kotlin/info/maila/baseapp/database/repository/TablePageableRepository.kt
+++ src/main/kotlin/info/maila/baseapp/database/repository/TablePageableRepository.kt
@@ -3,11 +3,17 @@ package info.maila.baseapp.database.repository
 import info.maila.baseapp.common.rest.TablePage
 import info.maila.baseapp.common.rest.TablePageable
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.data.domain.Sort
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate
-import org.springframework.data.relational.core.query.Criteria
-import org.springframework.data.relational.core.query.Query
+import org.springframework.data.jdbc.core.convert.JdbcConverter
+import org.springframework.data.relational.domain.RowDocument
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Component
+import java.sql.ResultSet
 import kotlin.reflect.KClass
+import kotlin.time.Duration
+import kotlin.time.TimedValue
+import kotlin.time.measureTime
 import kotlin.time.measureTimedValue
 
 interface TablePageableRepository<T : Any> {
@@ -19,75 +25,101 @@ interface TablePageableRepository<T : Any> {
 @Component
 class TablePageableRepositoryImpl<T : Any>(
     private val jdbcAggregateTemplate: JdbcAggregateTemplate,
-    private val entityFieldCache: EntityFieldCache
-) :
-    TablePageableRepository<T> {
+    private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate,
+    private val jdbcConverter: JdbcConverter,
+    private val entityCache: EntityCache
+) : TablePageableRepository<T> {
 
     private val logger = KotlinLogging.logger { }
 
     override fun findAll(pageable: TablePageable, entityClass: KClass<T>): TablePage<T> {
 
-        val criteria = buildCriteria(pageable, entityClass)
-        var query = Query.query(criteria)
-        query = query.limit(pageable.limit)
-        query = query.offset(pageable.offset)
-        if (pageable.sort != null) {
-            query = query.sort(pageable.sort)
-        }
-
-        val (entities, entitiesRuntime) = measureTimedValue {
-            jdbcAggregateTemplate.findAll(query, entityClass.java)
-        }
-        val (total, totalRuntime) = measureTimedValue {
-            jdbcAggregateTemplate.count(Query.query(criteria), entityClass.java)
-        }
-        val (totalNonFiltered, totalNonFilteredRuntime) = measureTimedValue {
-            jdbcAggregateTemplate.count(entityClass.java)
-        }
-
+        val (sql, countSql) = buildSql(pageable, entityClass)
+        logger.debug { "findAll<${entityClass.simpleName}> SQL: $sql" }
+        val (entities, queryRuntime) = query(sql, entityClass)
+        val (total, countRuntime) = count(countSql)
+        val (totalNonFiltered, countTotalRuntime) = countAll(entityClass)
         val page = TablePage(
-            entities.toList(),
-            total,
-            totalNonFiltered,
-            entitiesRuntime,
-            totalRuntime,
-            totalNonFilteredRuntime
+            rows = entities, queryRuntime = queryRuntime,
+            total = total, countRuntime = countRuntime,
+            totalNonFiltered = totalNonFiltered, countTotalRuntime = countTotalRuntime
         )
-        logger.debug { "findAll<${entityClass.simpleName}>($pageable): ${query.debugString()} -> $page" }
+        logger.trace { "findAll<${entityClass.simpleName}> $page <- $pageable" }
         return page
-
     }
 
-    private fun buildCriteria(pageable: TablePageable, entityClass: KClass<T>): Criteria {
-        var criteria = Criteria.empty() //.where("id").isNotNull
+    private fun buildSql(
+        pageable: TablePageable,
+        entityClass: KClass<T>
+    ): Pair<String, String> {
+        val tableName = entityCache.get(entityClass).tableName.reference
+        val fields = "*"
+        val whereClauses = mutableListOf<String>()
         val search = pageable.search
         if (search != null) {
-            val isNumber = search.isNumber()
-            pageable.fields.forEach { field ->
-                val entityField = entityFieldCache.get(entityClass, field)
-                if (entityField.isString) {
-                    criteria = criteria.or(entityField.column).like("%$search%")
+            val searchLower = search.lowercase()
+            pageable.fields
+                .filter { field -> entityCache.get(entityClass, field).isString }
+                .forEach { field ->
+                    whereClauses += "LOWER(\"$field\") LIKE '%$searchLower%'"
                 }
+        }
+        val whereClause = whereClauses.joinToString(separator = " OR ", prefix = " WHERE ") {
+            "($it)"
+        }
+        val sort = pageable.sort
+        val orderClause = if (sort != null && sort.isSorted) {
+            val sortList: List<Sort.Order> = sort.toList()
+            sortList.joinToString(separator = ", ", prefix = " ORDER BY ") {
+                "${it.property} ${it.direction.name}"
+            }
+        } else ""
+        val sql = "" +
+                "SELECT $fields " +
+                "FROM \"$tableName\"" +
+                "$whereClause " +
+                "$orderClause " +
+                "OFFSET ${pageable.offset} " +
+                "LIMIT ${pageable.limit}"
+        val countSql = "" +
+                "SELECT COUNT (*) " +
+                "FROM \"$tableName\"" +
+                whereClause
+        return Pair(sql, countSql)
+    }
+
+    private fun query(sql: String, entityClass: KClass<T>): Pair<List<T>, Duration> {
+        val entities = mutableListOf<T>()
+        val runtime = measureTime {
+            namedParameterJdbcTemplate.query(sql, paramMap) { rs: ResultSet ->
+                val metaData = rs.metaData
+                entities += jdbcConverter.read(
+                    entityClass.java,
+                    RowDocument(
+                        (1..metaData.columnCount)
+                            .mapNotNull(metaData::getColumnName)
+                            .associateWith(rs::getObject)
+                    )
+                )
             }
         }
-        return criteria
+        return Pair(entities, runtime)
     }
+
+    private fun count(sql: String): Pair<Long, Duration> {
+        var total: Long = 0
+        val runtime = measureTime {
+            namedParameterJdbcTemplate.query(sql, paramMap) { rs: ResultSet ->
+                total = rs.getLong(1)
+            }
+        }
+        return Pair(total, runtime)
+    }
+
+    private fun countAll(entityClass: KClass<T>): TimedValue<Long> = measureTimedValue {
+        jdbcAggregateTemplate.count(entityClass.java)
+    }
+
+    val paramMap = emptyMap<String, Any>()
 
 }
-
-private fun String.isNumber() =
-    try {
-        toDouble()
-        true
-    } catch (_: NumberFormatException) {
-        false
-    }
-
-private fun Query.debugString(): String =
-    listOf(
-        "criteria=$criteria",
-        "columns=$columns",
-        "sort=$sort",
-        "limit=$limit",
-        offset
-    ).joinToString(", ", prefix = "Query(", postfix = ")") { it.toString() }
